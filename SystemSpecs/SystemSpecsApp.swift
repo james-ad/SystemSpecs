18bf87118c050c7f7b39d73baa5ccbaa22a9f80a
james-du <muzikjay@gmail.com>
2023-06-05 19:56:09 -0700

    Initial Commit

---
+++ SystemSpecs/SystemSpecsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemSpecsApp.swift
+//  SystemSpecs
+//
+//  Created by James Dunn on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SystemSpecsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
